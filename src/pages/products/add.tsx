e62df8ff0e574e482e07fdfce44dc8bd37aa8c58
Phu Bhutia <pbhu@equinor.com>
2023-02-14 12:38:26 +0530

    Error regarding ' fixed

--- src/pages/products/add.tsx
+++ src/pages/products/add.tsx
@@ -170,7 +170,7 @@ function AddProduct() {
                       </AlertDialogHeader>
 
                       <AlertDialogBody>
-                        Are you sure? You can't undo this action afterwards.
+                        Are you sure? You can&apos;t undo this action afterwards.
                       </AlertDialogBody>
 
                       <AlertDialogFooter>
